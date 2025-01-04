674b85d75a1a2f7c1c0d924c37a10de4f827f636
WCL0304 <1463771821@qq.com>
2025-01-05 06:44:50 +0800

    hw6 try

---
+++ codes/hw6/newdriver.m
@@ -0,0 +1,35 @@
+clear;
+clc;
+close all;
+
+kappa = 1.0; % conductivity
+
+% exact solution
+exact = @(x,y) x*(1-x)*y*(1-y);
+exact_x = @(x,y) (1-2*x)*y*(1-y);
+exact_y = @(x,y) x*(1-x)*(1-2*y);
+
+f = @(x,y) 2.0*kappa*x*(1-x) + 2.0*kappa*y*(1-y); % source term
+
+% quadrature rule
+n_int_xi  = 3;
+n_int_eta = 3;
+n_int     = n_int_xi * n_int_eta;
+[xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);
+
+% mesh generation
+n_en   = 4;               % number of nodes in an element
+n_el_x = 60;               % number of elements in x-dir
+n_el_y = 60;               % number of elements in y-dir
+n_el   = n_el_x * n_el_y; % total number of elements
+
+n_np_x = n_el_x + 1;      % number of nodal points in x-dir
+n_np_y = n_el_y + 1;      % number of nodal points in y-dir
+n_np   = n_np_x * n_np_y; % total number of nodal points
+
+x_coor = zeros(n_np, 1);
+y_coor = x_coor;
+
+hx = 1.0 / n_el_x;        % mesh size in x-dir
+hy = 1.0 / n_el_y;        % mesh size in y-dir
+
