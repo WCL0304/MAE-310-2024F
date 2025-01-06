6935159adb6d569b0364004f53aaae90dc7be3d0
WCL0304 <1463771821@qq.com>
2025-01-06 17:53:41 +0800

    try

--- codes/hw6/newdriver.m
+++ codes/hw6/newdriver.m
@@ -33,20 +33,39 @@ y_coor = x_coor;
 hx = 1.0 / n_el_x;        % mesh size in x-dir
 hy = 1.0 / n_el_y;        % mesh size in y-dir
 
-% mesh generation
-n_el_x_values = [10, 20, 40, 80, 160];
-n_el_y_values = n_el_x_values;
+% generate the nodal coordinates
+for ny = 1 : n_np_y
+  for nx = 1 : n_np_x
+    index = (ny-1)*n_np_x + nx; % nodal index
+    x_coor(index) = (nx-1) * hx;
+    y_coor(index) = (ny-1) * hy;
+  end
+end
+% IEN array for triangles
+IEN = zeros(2*n_el, 3); % 每个四边形元素被分成两个三角形元素
+for ex = 1 : n_el_x
+    for ey = 1 : n_el_y
+        ee = (ey-1) * n_el_x + ex; % 四边形元素索引
+        IEN(2*(ee-1)+1, :) = [(ey-1) * n_np_x + ex, (ey-1) * n_np_x + ex + 1, ey * n_np_x + ex + 1]; % 第一个三角形元素
+        IEN(2*ee, :) = [(ey-1) * n_np_x + ex, ey * n_np_x + ex + 1, ey * n_np_x + ex]; % 第二个三角形元素
+    end
+end
 
-% 存储误差
-errors_L2_quad = zeros(size(n_el_x_values));
-errors_H1_quad = zeros(size(n_el_x_values));
-errors_L2_tri = zeros(size(n_el_x_values));
-errors_H1_tri = zeros(size(n_el_x_values));
+% ID array for triangles
+ID = zeros(n_np, 1);
+counter = 0;
+for ny = 2 : n_np_y - 1
+    for nx = 2 : n_np_x - 1
+        index = (ny-1)*n_np_x + nx;
+        counter = counter + 1;
+        ID(index) = counter;
+    end
+end
+n_eq = counter;
 
-% 制造的解
-manufactured_solution = @(x, y) x.^2 .* y.^2;
+LM = ID(IEN);
 
-% 制造的解的梯度
-manufact_solution_grad_x = @(x, y) 2 * x .* y.^2;
-manufact_solution_grad_y = @(x, y) 2 * y .* x.^2;
+% allocate the stiffness matrix and load vector
+K = spalloc(n_eq, n_eq, 9 * n_eq);
+F = zeros(n_eq, 1);
 
