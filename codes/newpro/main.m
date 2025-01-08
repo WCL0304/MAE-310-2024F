f4e1c1abb803293545b78bb1acf4826f7fb7a65b
WCL0304 <1463771821@qq.com>
2025-01-08 15:11:03 +0800

    man

--- codes/newpro/main.m
+++ codes/newpro/main.m
@@ -3,12 +3,16 @@ clear; clc;
 
 % 读取网格数据
 [nodes, elements, boundaryEdges] = read_gmsh_mesh('quarter-plate-with-hole-quad.msh');
+disp(['节点数组大小: ', num2str(size(nodes, 1)), ' x ', num2str(size(nodes, 2))]); % 调试信息
+disp(['边界元素数组大小: ', num2str(size(boundaryEdges, 1)), ' x ', num2str(size(boundaryEdges, 2))]); % 调试信息
+disp('边界元素内容:');
+disp(boundaryEdges); % 打印边界元素的具体内容
 
 % 材料参数
 E = 1e11; % 弹性模量
 nu = 0.3; % 泊松比
-L = 4;
-R = 0.5;
+L = 4; % 假设 L 为 4
+
 % 模型选择
 isPlaneStress = true; % 平面应力模型
 
@@ -61,37 +65,5 @@ visualize_results(nodes, displacements, strains, stresses);
 % 计算误差
 exact_solution = @(x, y, type) exact_solution_helper(x, y, L, R, type);
 [L2_error, H1_error] = calculate_errors(nodes, displacements, exact_solution);
-disp(['L2 范数误差: ', num2str(L2_error)]);
-disp(['H1 范数误差: ', num2str(H1_error)]);
-
-
-function [u, v] = exact_solution_helper(x, y, L, R, type)
-    % 计算解析解
-    T = 10e3; % 牵引力
-    r = sqrt(x.^2 + y.^2);
-    theta = atan2(y, x);
-
-    if type == 'u'
-        u = T * (1 - R^2 / r^2) * cos(theta);
-    elseif type == 'v'
-        v = T * (1 - R^2 / r^2) * sin(theta);
-    end
-end
-
-function strains = calculate_strains(displacements, nodes, elements)
-    % 计算应变
-    nElements = size(elements, 1);
-    strains = zeros(nElements, 3);
-    for i = 1:nElements
-        element = elements(i, :);
-        disp_elem = displacements(2 * (element - 1) + [1, 2], :);
-        [B, detJ] = strain_displacement_matrix(element, nodes, [0, 0]);
-        strains(i, :) = B * disp_elem / detJ;
-    end
-end
-
-function stresses = calculate_stresses(strains, E, nu, isPlaneStress)
-    % 计算应力
-    D = plane_stress_strain_matrix(E, nu, isPlaneStress);
-    stresses = D * strains';
-end
+disp(['L2范数误差: ', num2str(L2_error)]);
+disp(['H1范数误差: ', num2str(H1_error)]);
