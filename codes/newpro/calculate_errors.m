44161e97074868d83a735e7c22ee34ce392291c9
WCL0304 <1463771821@qq.com>
2025-01-08 15:08:21 +0800

    .

--- codes/newpro/calculate_errors.m
+++ codes/newpro/calculate_errors.m
@@ -1,23 +1,26 @@
+% 计算误差
 function [L2_error, H1_error] = calculate_errors(nodes, displacements, exact_solution)
-    % 计算 L2 和 H1 范数误差
-    nNodes = size(nodes, 1);
-    u = displacements(:, 1);
-    v = displacements(:, 2);
-    u_exact = exact_solution(nodes, 'u');
-    v_exact = exact_solution(nodes, 'v');
-
+    % 计算 u 和 v 的精确解
+    u_exact = arrayfun(@(x, y) exact_solution(x, y, 'u'), nodes(:, 1), nodes(:, 2));
+    v_exact = arrayfun(@(x, y) exact_solution(x, y, 'v'), nodes(:, 1), nodes(:, 2));
+    
+    % 提取计算解
+    u = displacements(1:2:end);
+    v = displacements(2:2:end);
+    
     % L2 范数误差
     L2_error_u = norm(u - u_exact) / norm(u_exact);
     L2_error_v = norm(v - v_exact) / norm(v_exact);
     L2_error = mean([L2_error_u, L2_error_v]);
-
+    
+    % 计算梯度
+    [dudx, dudy] = gradient(u, nodes(:, 1), nodes(:, 2));
+    [dvdx, dvdy] = gradient(v, nodes(:, 1), nodes(:, 2));
+    [dudx_exact, dudy_exact] = gradient(u_exact, nodes(:, 1), nodes(:, 2));
+    [dvdx_exact, dvdy_exact] = gradient(v_exact, nodes(:, 1), nodes(:, 2));
+    
     % H1 范数误差
-    grad_u = gradient(u, nodes(:, 1), nodes(:, 2));
-    grad_v = gradient(v, nodes(:, 1), nodes(:, 2));
-    grad_u_exact = gradient(u_exact, nodes(:, 1), nodes(:, 2));
-    grad_v_exact = gradient(v_exact, nodes(:, 1), nodes(:, 2));
-
-    H1_error_u = norm([grad_u(1) - grad_u_exact(1); grad_u(2) - grad_u_exact(2)]) / norm(grad_u_exact);
-    H1_error_v = norm([grad_v(1) - grad_v_exact(1); grad_v(2) - grad_v_exact(2)]) / norm(grad_v_exact);
+    H1_error_u = norm([dudx - dudx_exact; dudy - dudy_exact]) / norm([dudx_exact; dudy_exact]);
+    H1_error_v = norm([dvdx - dvdx_exact; dvdy - dvdy_exact]) / norm([dvdx_exact; dvdy_exact]);
     H1_error = mean([H1_error_u, H1_error_v]);
 end
